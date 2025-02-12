5d2db5c58d369a38f2dec7365bf902faf0c1ce5e
mazam5 <mohammedazam651@gmail.com>
2025-02-12 20:21:17 +0530

    fixing: issues

--- backend/src/middleware/middleware_auth.js
+++ backend/src/middleware/middleware_auth.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/model_user.js";
-
+import User from "../models/user-model.js";
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.header("Authorization")?.split(" ")[1];
--- backend/src/services/service_auth.js
+++ backend/src/services/service_auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/Model_User.js";
+import User from "../models/user-model.js";
 
 export const registerUser = async ({ username, email, password }) => {
   const hashedPassword = await bcrypt.hash(password, 10);
